e500f85adb5086cd9e8678ebc3439a8aefb08f66
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-31 20:34:15 +0700

    create table user_membership

---
+++ database/migrations/2019_08_31_132808_create_user_membership_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserMembershipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_membership', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->bigInteger('user_id')->unsigned();
+          $table->bigInteger('membership_id')->unsigned();
+          $table->string('credit_card');
+
+          $table->foreign('user_id')->references('id')
+                ->on('users')->onDelete('cascade');
+
+          $table->foreign('membership_id')->references('id')
+                ->on('memberships')->onDelete('cascade');
+
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_membership');
+    }
+}

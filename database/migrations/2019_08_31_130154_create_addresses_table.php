7a6217723e332275cc40bb14fea07a0963c6bbda
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-31 20:13:12 +0700

    create table addresses

---
+++ database/migrations/2019_08_31_130154_create_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('address');
+            $table->string('city',100);
+            $table->string('province',100);
+            $table->string('postal',5);
+            $table->string('country',100);
+            $table->integer('is_default')->length(1);
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
